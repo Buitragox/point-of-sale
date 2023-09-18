ebf1c83eea8c9796fa90c6334d4aead5e828d58a
Buitragox <jhoanuitrago@gmail.com>
2023-09-17 21:46:44 -0500

    Erase whitespaces

--- models/product_sale.py
+++ models/product_sale.py
@@ -10,10 +10,3 @@ class ProductSale(db.Model):
     sale_id = Column("sale_id", UUID, ForeignKey("sales.sale.sale_id"), primary_key=True)
     product_id = Column("product_id", Integer, ForeignKey("inventory.product.product_id"), primary_key=True)
     amount = Column("amount", Integer)
-
-    # def __init__(self, sale_id, product_id, amount):
-    #     self.sale_price = sale_id
-    #     self.seller_id = product_id
-    #     self.client_id = amount
-    
-
