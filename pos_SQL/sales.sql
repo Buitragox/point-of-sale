eab606b7abc66856c9ab85174baad6ac18f8926b
Buitragox <jhoanuitrago@gmail.com>
2023-09-17 21:46:33 -0500

    Fix primary keys

--- pos_SQL/sales.sql
+++ pos_SQL/sales.sql
@@ -10,6 +10,7 @@ CREATE TABLE IF NOT EXISTS sales.product_sale(
     sale_id UUID NOT NULL,
     product_id INTEGER NOT NULL,
     amount INTEGER,
+    CONSTRAINT pk_sale_product PRIMARY KEY (sale_id, product_id),
     CONSTRAINT fk_sale_id FOREIGN KEY (sale_id) REFERENCES sales.sale(sale_id),
     CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES inventory.product(product_id)
 );
